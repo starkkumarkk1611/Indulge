2e1b2cb3a11d2b55e78f5d0ed6fe5a9a87bc8c14
Kundan <starkkumarkk1611@gmail.com>
2022-02-17 02:32:04 +0530

    Link error coorected

--- indulge-client/src/pages/landing/Home.jsx
+++ indulge-client/src/pages/landing/Home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import { Link } from "react-router-dom";
-import heritagebuilding from '../../assets/image/heritage.png'
-import './Home.css'
+import heritagebuilding from "../../assets/image/heritage.png";
+import "./Home.css";
 
 const Home = () => {
   return (
@@ -11,9 +11,12 @@ const Home = () => {
       <div id="heritage">
         <div id="page">
           <div id="text">
-
-            <h1>A ONE STOP PORTAL<br /> FOR PLACEMENTS AT<br />IIT(ISM) DHANBAD</h1>
-
+            <h1>
+              A ONE STOP PORTAL
+              <br /> FOR PLACEMENTS AT
+              <br />
+              IIT(ISM) DHANBAD
+            </h1>
           </div>
           <div id="login">
             <div className="logIn" id="login-student">
@@ -23,22 +26,11 @@ const Home = () => {
               <Link to="/login/recuiter">Login As Recuiter</Link>
             </div>
             <div className="logIn" id="login-admin">
-              <Link to="/login/recuiter">Login As Admin</Link>
+              <Link to="/login/admin">Login As Admin</Link>
             </div>
-
           </div>
         </div>
       </div>
-
-      <div id="cards">
-        yash
-      </div>
-
-      <Link to="/login/student">Login As Student</Link> <br />
-      <Link to="/login/recuiter">Login As Recuiter</Link> <br />
-      <Link to="/login/admin">Login As Admin</Link> <br /> <br />
-      <Link to="/signup/student">Signup As Student</Link> <br />
-      <Link to="/signup/recuiter">Signup As Recuiter</Link> <br />
     </div>
   );
 };
